03e38a84074701289810608c9357c18556387c93
bhargava ganti <bhargavaganthi@gmail.com>
2019-12-14 15:02:14 +0530

    clean data

--- soko_crud/settings.py
+++ soko_crud/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '^v$a(&d@1r%%s@nr!sgk523tj2i(7=@vo5#n-$u@*t=rh!z407'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
